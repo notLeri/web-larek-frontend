d1f42efa3f7d5e07f6bd06a5c02213db373d8009
leri <ed.nazarov001@gmail.com>
2024-09-17 00:02:23 +0300

    added basket to model

--- src/components/model/basket.ts
+++ src/components/model/basket.ts
@@ -2,18 +2,27 @@ import { IItemAPI, IListAPI, IBasket, IBasketItem } from '../../types/index';
 import { productAPI } from './apiHandler';
 
 class Basket implements IBasket {
-    constructor(private items: IItemAPI[] = []) {}
+    constructor(
+        private items: IItemAPI[] = [], 
+        private basketItems: IBasketItem[] = []
+    ) {}
 
     getItems(): IBasketItem[] {
-
+        return this.basketItems;
     }
 
     add(id: string): void {
-
+        const product = this.items.find(item => item.id === id);
+        if (!product) {
+            this.basketItems.push(product);
+        }
     }
 
     remove(id: string): void {
-
+        const product = this.items.find(item => item.id === id);
+        if (product) {
+            this.basketItems.filter(basketItem => basketItem.id !== id);
+        }
     }
 
     updateState(): Promise<void> {
